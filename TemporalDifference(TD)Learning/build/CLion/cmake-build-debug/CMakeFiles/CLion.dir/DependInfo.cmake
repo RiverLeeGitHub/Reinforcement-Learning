bd17bb48e86d80c56843c08b3d94d7e37c8929eb
Jiang Li <lijiang_1998@qq.com>
2019-12-24 23:49:41 -0500

    Add files via upload

---
+++ TemporalDifference(TD)Learning/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/Acrobot.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/Acrobot.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/CartPole.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/CartPole.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/FourierBasis.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/FourierBasis.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/Gridworld.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/Gridworld.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/MathUtils.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/MathUtils.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/MountainCar.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/MountainCar.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/QLearning.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/QLearning.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/Sarsa.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/Sarsa.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/main.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/main.cpp.o"
+  "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/main_test.cpp" "/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/build/CLion/cmake-build-debug/CMakeFiles/CLion.dir/Users/jiangli/Documents/UMassAmherst/687/HW4/HW4Source/HW4/src/main_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../header"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
